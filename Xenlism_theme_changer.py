fa0055561d3b0d6d949f0adea9237b48852c1424
Simarra <loic.martel@outlook.Com>
2016-11-05 22:56:07 +0100

    Create Xenlism_theme_changer.py

---
+++ Xenlism_theme_changer.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+#-*-coding:utf-8-*-
+from time import localtime
+from subprocess import call
+
+class c_theme_changer:
+
+    def __init__(self):
+
+        self.themeday = {'0':'Xenlism-Wildfire-MonDay',
+        '1':'Xenlism-Wildfire-TuesDay',
+        '2':'Xenlism-Wildfire-WednesDay',
+        '3': 'Xenlism-Wildfire-ThursDay',
+        '4' : 'Xenlism-Wildfire-FriDay',
+        '5': 'Xenlism-Wildfire-SaturDay',
+        '6': 'Xenlism-Wildfire-SunDay'}
+
+
+
+
+    def get_date_day(self):
+        self.the_day = str(localtime()).split(' ')[2].split('=')[1].replace(',','')
+        return self.the_day
+
+    def get_match(self):
+        print ('voici le match')
+        self.matching = self.themeday[self.get_date_day()]
+        return self.matching
+        
+    def change_icons(self):
+        call ("gsettings set org.gnome.desktop.interface icon-theme {0}".format(self.get_match()),shell=True)
+
+
+c_theme_changer().change_icons()
