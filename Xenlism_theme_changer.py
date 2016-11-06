8e23baf4d5cf5cc3f4a7aa3526cace020c3b0791
Simarra <loic.martel@outlook.Com>
2016-11-06 10:48:10 +0100

    Update Xenlism_theme_changer.py

--- Xenlism_theme_changer.py
+++ Xenlism_theme_changer.py
@@ -30,6 +30,6 @@ class c_theme_changer:
     def change_icons(self):
         call ("gsettings set org.gnome.desktop.interface icon-theme {0}".format(self.get_match()),shell=True)
 
-if name == '__main__'
+if __name__ == '__main__':
     c_theme_changer().change_icons()
 
